ef2e8f6c3e6adc519521cf9e115ab88062d72861
paragpatil187 <91532920+paragpatil187@users.noreply.github.com>
2021-12-13 17:12:42 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,7 @@ const router= express.Router();
 const User = require("../models/user.model");
 router.get("",async(req,res)=>{
     try{
-        const User =await User.find().lean().exec();
+        const user =await User.find().lean().exec();
         res.status(200).send(user)
 
     }catch(e){
